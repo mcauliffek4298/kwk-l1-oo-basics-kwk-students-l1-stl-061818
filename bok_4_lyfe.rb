812052d9337b6f83779c8ade3e2590cdcb864b21
mcauliffek4298 <kathryn@peerlift.org>
2018-06-22 16:46:52 +0000

    automatically backed up by learn

--- bok_4_lyfe.rb
+++ bok_4_lyfe.rb
@@ -31,15 +31,18 @@ class Bok_4_Lyfe
     puts "Brianna's eye color is #{brianna.eyecolors}."
     puts "Brianna's favorite movie is #{brianna.movies}."
     
-    # bella = Bok_4_Lyfe.new ("dance", "blue", "The Star Wars Trilogies")
-    # puts "Give me some information about Bella!"
-    #   puts "bellas actvity is #{bella.activity}."
-    #   puts "bellas eye color is #{brianna.eye_color}."
-    #   puts "bellas favorite movie is #{brianna.movies}"
+    bella = Bok_4_Lyfe.new ("dance", "blue", "The Star Wars Trilogies")
+      puts "Give me some information about Bella!"
+      puts "bella is a #{bella.gender}"
+      puts "bellas actvity is #{bella.activity}."
+      puts "bellas eye color is #{bella.eyecolors}."
+      puts "bellas favorite movie is #{bella.movies}"
       
-    # kathryn = Bok_4_Lyfe.new ("cheer", "blue", "Dead Poet's Society")
-    #   puts "Give me some information about Kathryn!"
-    #   puts "kathryns activity is #{kathryn.activity}."
-    #   puts "kathryns eye color is #{kathryn.eye_color}."
-    #   puts "kathryns favorite movie is #{kathryn.movies}."
+    kathryn = Bok_4_Lyfe.new ("cheer", "blue", "Dead Poet's Society")
+      puts "Give me some information about Kathryn!"
+      puts "kathryn is a #{kathryn.gender}."
+      puts "kathryns activity is #{kathryn.activity}."
+      puts "kathryns eye color is #{kathryn.eyecolor}."
+      puts "kathryns favorite movie is #{kathryn.movies}."
+      puts "kathryns favorite movie is #{kathryn.movies}."
     end
