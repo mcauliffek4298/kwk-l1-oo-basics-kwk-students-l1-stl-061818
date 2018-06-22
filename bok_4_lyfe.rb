2c473248ac96a2d8ea62573e21e10c056bd1f4af
mcauliffek4298 <kathryn@peerlift.org>
2018-06-22 16:52:22 +0000

    automatically backed up by learn

--- bok_4_lyfe.rb
+++ bok_4_lyfe.rb
@@ -7,11 +7,7 @@ class Bok_4_Lyfe
     @movies = movies
   end
   
-  
-  
-  
-  
-  def gender
+    def gender
     @gender
   end
   
@@ -26,25 +22,21 @@ class Bok_4_Lyfe
   def movies
     @movies
   
-  brianna = Bok_4_Lyfe.new ("girl", "track","eye color", "50 First Dates")
+  brianna = Bok_4_Lyfe.new ("track","eyecolors", "50 First Dates")
     puts "Give me some information about Brianna!"
-    puts "Brianna is a #{brianna.gender}"
     puts "Briannas actvity is #{brianna.activity}."
     puts "Brianna's eye color is #{brianna.eyecolors}."
     puts "Brianna's favorite movie is #{brianna.movies}."
     
     bella = Bok_4_Lyfe.new ("dance", "blue", "The Star Wars Trilogies")
       puts "Give me some information about Bella!"
-      puts "bella is a #{bella.gender}"
       puts "bellas actvity is #{bella.activity}."
       puts "bellas eye color is #{bella.eyecolors}."
       puts "bellas favorite movie is #{bella.movies}"
       
     kathryn = Bok_4_Lyfe.new ("cheer", "blue", "Dead Poet's Society")
       puts "Give me some information about Kathryn!"
-      puts "kathryn is a #{kathryn.gender}."
       puts "kathryns activity is #{kathryn.activity}."
       puts "kathryns eye color is #{kathryn.eyecolor}."
-      puts "kathryns favorite movie is #{kathryn.movies}."
       puts "kathryns favorite movie is #{kathryn.movies}."
     end
