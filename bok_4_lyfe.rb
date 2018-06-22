b55c613ff40cc303ed74de23b38391b74670d8f6
mcauliffek4298 <kathryn@peerlift.org>
2018-06-22 16:57:14 +0000

    automatically backed up by learn

--- bok_4_lyfe.rb
+++ bok_4_lyfe.rb
@@ -29,19 +29,19 @@ class Bok_4_Lyfe
   end
 end
   
-  brianna = Bok_4_Lyfe.new ("track","eyecolors", "50 First Dates")
+  brianna = Bok_4_Lyfe.new("track","eyecolors", "50 First Dates")
     puts "Give me some information about Brianna!"
     puts "Briannas actvity is #{brianna.activity}."
     puts "Brianna's eye color is #{brianna.eye_color}."
     puts "Brianna's favorite movie is #{brianna.movies}."
     
-    bella = Bok_4_Lyfe.new ("dance", "blue", "The Star Wars Trilogies")
+    bella = Bok_4_Lyfe.new("dance", "blue", "The Star Wars Trilogies")
       puts "Give me some information about Bella!"
       puts "bellas actvity is #{bella.activity}."
       puts "bellas eye color is #{bella.eye_color}."
       puts "bellas favorite movie is #{bella.movies}"
       
-    kathryn = Bok_4_Lyfe.new ("cheer", "blue", "Dead Poet's Society")
+    kathryn = Bok_4_Lyfe.new("cheer", "blue", "Dead Poet's Society")
       puts "Give me some information about Kathryn!"
       puts "kathryns activity is #{kathryn.activity}."
       puts "kathryns eye color is #{kathryn.eye_color}."
