7e5bc5623a121647e98cae5a9148944e26578671
mcauliffek4298 <kathryn@peerlift.org>
2018-06-22 16:56:31 +0000

    automatically backed up by learn

--- bok_4_lyfe.rb
+++ bok_4_lyfe.rb
@@ -1,7 +1,7 @@
 class Bok_4_Lyfe
  
-  def initialize(eyecolors, activity, movies)
-    @eyecolors = eyecolors
+  def initialize(eye_color, activity, movies)
+    @eye_color = eye_color
     @activity = activity
     @movies = movies
   end
@@ -13,12 +13,10 @@ class Bok_4_Lyfe
     puts "she is a part of kode with klossy"
     puts "she is a girl"
     
-    def gender
-    @gender
   end
   
-  def eyecolors
-    @eyecolors
+  def eye_color
+    @eye_color
   end
   
   def activity
@@ -31,18 +29,18 @@ class Bok_4_Lyfe
   brianna = Bok_4_Lyfe.new ("track","eyecolors", "50 First Dates")
     puts "Give me some information about Brianna!"
     puts "Briannas actvity is #{brianna.activity}."
-    puts "Brianna's eye color is #{brianna.eyecolors}."
+    puts "Brianna's eye color is #{brianna.eye_color}."
     puts "Brianna's favorite movie is #{brianna.movies}."
     
     bella = Bok_4_Lyfe.new ("dance", "blue", "The Star Wars Trilogies")
       puts "Give me some information about Bella!"
       puts "bellas actvity is #{bella.activity}."
-      puts "bellas eye color is #{bella.eyecolors}."
+      puts "bellas eye color is #{bella.eye_color}."
       puts "bellas favorite movie is #{bella.movies}"
       
     kathryn = Bok_4_Lyfe.new ("cheer", "blue", "Dead Poet's Society")
       puts "Give me some information about Kathryn!"
       puts "kathryns activity is #{kathryn.activity}."
-      puts "kathryns eye color is #{kathryn.eyecolor}."
+      puts "kathryns eye color is #{kathryn.eye_color}."
       puts "kathryns favorite movie is #{kathryn.movies}."
     end
